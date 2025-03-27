3964ffe853ac0e4b7dd2304457576c141f6353f2
Hawk3467hawkpapa <hawkinsoppong0@gmail.com>
2025-03-27 18:14:59 +0330

    Create script..js

---
+++ script..js
@@ -0,0 +1,11 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    if(email === "user@hawkins.com" && password === "password123") {
+        alert("Login successful! Welcome to Hawkins.");
+    } else {
+        alert("Invalid email or password. Please try again.");
+    }
+});
